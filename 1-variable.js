007daeb4be82abddd05dfdafa606a462b15a62d6
cifucarst <cifentesparraandres@gmail.com>
2023-11-26 13:25:44 -0500

    Variables in JavaScript

---
+++ 1-variable.js
@@ -0,0 +1,13 @@
+let andyBox = "woody";
+// declaration and initialization
+console.log(andyBox);
+
+// not allowed
+let c = "woody"; // not a descriptive variable
+let cda = "woody"; // also not descriptive
+let pcAndy = "woody"; // leads to ambiguities
+
+// allowed
+let andysFirstTrashCan = "Woody";
+let userUrl = "https://www.google.com";
+let userId = "12345676543";
