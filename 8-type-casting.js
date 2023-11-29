852df6797a1885af55064380aba314b7284afe05
cifucarst <cifentesparraandres@gmail.com>
2023-11-29 14:04:53 -0500

    type casting

---
+++ 8-type-casting.js
@@ -0,0 +1,30 @@
+// Explicit type casting
+
+const string = '42';
+const integer = parseInt(string);
+console.log(integer);
+console.log(typeof integer);
+
+const stringDecimal = '3.14';
+const float = parseFloat(stringDecimal);
+console.log(float);
+console.log(typeof float);
+
+const binary = '1010';
+const decimal = parseInt(binary, 2);
+console.log(decimal);
+console.log(typeof decimal);
+
+// Implicit type casting
+const sum = '5' + 3;
+console.log(sum);
+
+const sumWithBoolean = '3' + true;
+console.log(sumWithBoolean);
+
+const sumWithNumber = 2 + true;
+console.log(sumWithNumber);
+
+const stringValue = '10';
+const numberValue = 10;
+const booleanValue = true;
