903dd591b76d1be52e630ea27cc1b9edd9c44a3a
cifucarst <cifentesparraandres@gmail.com>
2023-11-29 13:57:31 -0500

    booleans

---
+++ 6-boolean.js
@@ -0,0 +1,19 @@
+// Booleans
+
+const isActive = true;
+console.log(isActive);
+
+const hasPermission = false;
+
+// Implicit conversion
+const result = 5 > 3;
+console.log(result);
+
+const name = 'Platzi';
+console.log(!!name);
+
+
+// Explicit conversion
+const value = 0;
+const explicitBoolean = Boolean(value);
+console.log(explicitBoolean); //false
