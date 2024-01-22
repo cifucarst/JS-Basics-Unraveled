6026ef62ecdef3fc776a94d113bc769e4c777480
cifucarst <cifentesparraandres@gmail.com>
2024-01-22 04:38:22 -0500

    Comparison operators

---
+++ 21-comparison-operators.js
@@ -0,0 +1,19 @@
+// Comparison Operators
+
+// == equality in value
+// === equality in value and data type
+
+// != not equal, negation
+// !== not equal in value and data type
+// > greater than
+// < less than
+// >= greater than or equal
+// <= less than or equal
+
+// Defining variables a, b, and c with numerical values.
+const a = 10;
+const b = 20;
+const c = 10;
+
+// Using the == equality operator to compare the values of 'a' and 'b'.
+a == b;
