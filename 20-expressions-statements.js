9cefeea987c231b3601eb5da74f6be80ee144fbf
cifucarst <cifentesparraandres@gmail.com>
2024-01-22 04:35:57 -0500

    expressions vs statements

---
+++ 20-expressions-statements.js
@@ -0,0 +1,36 @@
+/*
+Expressions:
+
+An expression in JavaScript is any piece of code that produces a value.
+It can be as simple as a variable, a literal value, or a more complex operation.
+Examples of simple expressions: */
+5;           // Numeric literal
+"Hello";     // String literal
+true;        // Boolean literal
+myVariable;  // Variable
+2 + 3;       // Addition operation
+
+/*
+Statements:
+
+A statement in JavaScript is an instruction that performs an action and may or may not return a value.
+Unlike expressions, statements do not always produce a value.
+Examples of statements: */
+
+var x = 10;           // Variable declaration
+function myFunction() { // Function declaration
+  // Function code
+}
+if (condition) {       // If statement
+  // Code if the condition is true
+} else {               // Else statement
+  // Code if the condition is false
+}
+
+/*
+In summary, expressions produce values and can be combined to form
+more complex expressions, while statements perform actions in the
+program and affect its structure or execution flow. In many cases,
+JavaScript allows the use of expressions in contexts where statements are expected,
+and vice versa, which can sometimes cause confusion in distinguishing between the two concepts.
+*/
