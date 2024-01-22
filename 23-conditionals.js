928c81654fbff69e8cc4db234ba29d1e8fcde364
cifucarst <cifentesparraandres@gmail.com>
2024-01-22 04:42:50 -0500

    conditionals

---
+++ 23-conditionals.js
@@ -0,0 +1,15 @@
+let name = "Diego";
+
+// Conditional Statement:
+// If the value of 'name' is exactly equal to "Diego", execute the code inside the block.
+if (name === "Diego") {
+    console.log(`Hello ${name}`);
+}
+// If the value of 'name' is exactly equal to "Nico", execute the code inside this block.
+else if (name === "Nico") {
+    console.log(`Hello ${name}`);
+}
+// If none of the above conditions are met, execute the code inside this block.
+else {
+    console.log(`Name not found`);
+}
