4ab90d4d95e7d6868ac12cc1a09f2085725baa38
cifucarst <cifentesparraandres@gmail.com>
2024-01-22 22:39:41 -0500

    for loops

---
+++ 27-for-loops.js
@@ -0,0 +1,10 @@
+// Definition of a list of activities.
+let list = ["eat", "sleep", "code", "repeat"];
+
+// "for" loop iterating over each element in the list.
+// Starts with the variable i at 0, executes the code as long as i is less than the length of the list,
+// and increments i by 1 in each iteration.
+for (let i = 0; i < list.length; i++) {
+    // Prints each element of the list at the current position to the console.
+    console.log(list[i]);
+}
