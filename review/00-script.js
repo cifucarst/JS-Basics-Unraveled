cf681db36ded0e5fd1b39c537e04d9ebd0bd5ccb
cifucarst <cifentesparraandres@gmail.com>
2024-05-10 21:01:20 -0500

    review, variables, constants, comments, primitive data type

---
+++ review/00-script.js
@@ -0,0 +1,49 @@
+// EXERCISE 00-SYNTAX, VARIABLES, DATA TYPES, AND HELLO WORLD
+
+//-------------------------------------------------------------
+
+// JAVASCRIPT https://developer.mozilla.org/es/docs/Web/JavaScript
+
+//--------------------------------------------------------------
+
+// Single-line comment
+/* Multi-line 
+comment
+*/
+
+//---------------------------------------------------------------
+// - Create a variable (and a constant if the language supports it).
+
+let variable = 'this is a variable'
+const constant = 'this is a constant'
+
+//----------------------------------------------------------------
+// - Create variables representing all primitive data types of the language (strings, integers, booleans...).
+
+// STRING
+let text = 'string'            //string
+
+// NUMBER
+let integer = 37               //integer
+let float = 37.1               //floating point number
+
+// NULL
+let nullData = null            //null data
+
+// UNDEFINED
+let undefinedData = undefined  //undefined data
+
+// BOOLEAN
+let boolean = true             //boolean data
+let boolean1 = false           //boolean data
+
+// BIGINT
+let bigInt = 50n               //bigint data (very large number)
+
+// SYMBOL
+let symbol = Symbol('whatever')
+
+//----------------------------------------------------------------
+
+ //- Print to the console the text: "Hello, [and the name of your language]!"
+ console.log('Hello JavaScript!')
